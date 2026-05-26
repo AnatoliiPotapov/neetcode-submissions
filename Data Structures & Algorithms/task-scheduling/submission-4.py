9ec2cebaf0f1ce1763818a68ca768f1493cf5abd
Anatoly <anatolii.s.potapov@gmail.com>
2026-05-26 21:29:41 +0300

    Add: task-scheduling - submission-4

---
+++ Data Structures & Algorithms/task-scheduling/submission-4.py
@@ -0,0 +1,29 @@
+from collections import Counter, deque
+import heapq
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        cnt = Counter(tasks)
+        heap = [(-cnt, val) for val, cnt in cnt.items()]
+        heapq.heapify(heap)
+        cooldown = deque()
+        time = 0
+
+        while heap or cooldown:
+            time += 1
+
+            if heap:
+                elem = heapq.heappop(heap)
+                
+                if elem[0] < -1:
+                    cooldown.append((elem[0] + 1, elem[1], time + n))
+            #if not heap and cooldown:
+            #    time = cooldown[0][2]
+
+            # check if I can return something from cooldown
+            if cooldown:
+                if cooldown[0][2] <= time:
+                    el = cooldown.popleft()[:2]
+                    heapq.heappush(heap, el)
+
+        return time
